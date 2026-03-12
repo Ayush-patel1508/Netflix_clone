8729cfcbcb78ea71d34f271b9df16fd6d7011187
Ayush Patel <ap1582006@gmail.com>
2026-03-12 12:45:44 +0530

    some basic changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Navbar from './Navbar.jsx'
 import Trending from './Trending.jsx'
 import Reason from './Reason.jsx'
